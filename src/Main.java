512b363d658e410517adede9e26caf9434db556c
Amin_stors <amangulovzyrgalbek@gmail.com>
2024-11-01 11:15:16 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,22 @@ public class Main {
         EducationCenter college2 = new College("Колледж №2", "Австралия", LocalDate.of(2005, 1, 1), 2004);
 
         Student[] students = new Student[10];
-        students[0] = new Student("Студент1", 'М', school1, 3);
-        students[1] = new Student("Студент2", 'Ж', university1, 4);
-        students[2] = new Student("Студент3", 'М', college1, 2);
-        students[3] = new Student("Студент4", 'Ж', school2, 3);
-        students[4] = new Student("Студент5", 'М', university2, 5);
-        students[5] = new Student("Студент6", 'Ж', college2, 1);
-        students[6] = new Student("Студент7", 'М', school1, 2);
-        students[7] = new Student("Студент8", 'Ж', university1, 1);
-        students[8] = new Student("Студент9", 'М', college1, 3);
-        students[9] = new Student("Студент10", 'Ж', school2, 2);
+        students[0] = new Student("Студент1 ", 'М', school1, 3);
+        students[1] = new Student("Студент2 ", 'Ж', university1, 4);
+        students[2] = new Student("Студент3 ", 'М', college1, 2);
+        students[3] = new Student("Студент4 ", 'Ж', school2, 3);
+        students[4] = new Student("Студент  ", 'М', university2, 5);
+        students[5] = new Student("Студент6 ", 'Ж', college2, 1);
+        students[6] = new Student("Студент7 ", 'М', school1, 2);
+        students[7] = new Student("Студент8 ", 'Ж', university1, 1);
+        students[8] = new Student("Студент9 ", 'М', college1, 3);
+        students[9] = new Student("Студент10 ", 'Ж', school2, 2);
 
         for (Student student : students) {
             System.out.println("Имя: " + student.getName());
             System.out.println("Пол: " + student.getGender());
+            student.getStudentEducationCenter();
+            student.getStudentStudyingYear();
             System.out.println("место где он учится: " + student.getEducationCenter().getName());
             System.out.println("Страна в котором находится учебное заведение: " + student.getEducationCenter().getLocatedCountry());
             System.out.println("сколько лет/год учится студент: " + student.getYearOfStudy());
